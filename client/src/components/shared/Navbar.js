146c4b3c3539b0d2bb2ba8c7dc18490f0683c244
Colby Patterson <cpatt@byui.edu>
2022-06-20 16:08:35 -0600

    Created navbar component

---
+++ client/src/components/shared/Navbar.js
@@ -0,0 +1,45 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { AuthContext } from "../../providers/AuthProvider"
+
+const Navbar = () => {
+  const {user, logout} = useContext(AuthContext)
+  const renderNav = () => {
+    if(user) {
+      return (
+        <>
+          <Link to='/account'>
+            <li>Account</li>
+          </Link>
+          <li onClick={logout}>Log Out</li>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Link to='/login'>
+            <li>Log In</li>
+          </Link>
+          <Link to='/register'>
+            <li>Register</li>
+          </Link>
+        </>
+      )
+    }
+  }
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          {renderNav()}
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
