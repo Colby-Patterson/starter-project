c2bebc6449ba9745ca4de454064b4dc3681bba1b
Colby Patterson <cpatt@byui.edu>
2022-06-20 17:03:18 -0600

    Imports for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/shared/Navbar'
+import Home from './components/shared/Home'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import NoMatch from './components/shared/NoMatch'
 
 function App() {
   return (
