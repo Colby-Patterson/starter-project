bb9bff8f76def995f88efb29c5b71fda28f4c07a
Colby Patterson <cpatt@byui.edu>
2022-06-22 14:39:44 -0600

    Added useAxiosOnMount hook

---
+++ client/src/hooks/useAxiosOnMount.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+export const useAxiosOnMount = (url) => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(()=>{
+    
+  }, [])
+
+  const getData = async () => {
+    try {
+      let res = await axios.get(url)
+      setData(res.data)
+    } catch(err) {
+      setError({message: err.message, response: err.response})
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {data, loading, error}
+}
